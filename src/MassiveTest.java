80e991a34250bc32fb51398bfe12384707175314
MajesticNthg <majestic_nthg@mail.ru>
2023-12-04 06:11:47 +1000

    mod.3

--- src/MassiveTest.java
+++ src/MassiveTest.java
@@ -11,9 +11,9 @@ public class MassiveTest {
     @Test
     public void randomMassive() {
         Massive massive = new Massive();
-        int[] resultArray = massive.randomMassive(new int[]{2, 4, 8, -1});
+        int[] resultArray = massive.randomMassive(new int[]{1, 0, 1, 1});
 
-        int[] expectedArray = new int[]{-1, 2, 4, 8};
+        int[] expectedArray = new int[]{0, 1, 1, 1};
         assertTrue(java.util.Arrays.equals(resultArray, expectedArray)); // тест на правильную сортировку (конечный результат)
 
         for (int x = 0; x < resultArray.length; x++)
@@ -22,15 +22,14 @@ public class MassiveTest {
         boolean actual = resultArray.length > 0;
         assertTrue(actual); //  размер массива не должен быть нулевым
 
+        boolean palindrome = true;
         for (int z = 0, y = resultArray.length - 1; z < resultArray.length / 2; z++, y--) {
-            boolean palindrome;
-            if (resultArray[z] == resultArray[y]) {
-                palindrome = true;
-            } else {
+            if (resultArray[z] != resultArray[y])
                 palindrome = false;
-            }
-            assertFalse(palindrome); // массив не должен представлять из себя палиндром
+
         }
+            assertFalse(palindrome); // массив не должен представлять из себя палиндром
+
     }
 }
 
