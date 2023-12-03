0e3275f6ea692f09fdde4aeebff4ff3ae8528ebf
MajesticNthg <majestic_nthg@mail.ru>
2023-12-04 06:01:59 +1000

    mod.2

--- src/MassiveTest.java
+++ src/MassiveTest.java
@@ -11,13 +11,13 @@ public class MassiveTest {
     @Test
     public void randomMassive() {
         Massive massive = new Massive();
-        int[] resultArray = massive.randomMassive(new int[]{2, 4, 8, 1});
+        int[] resultArray = massive.randomMassive(new int[]{2, 4, 8, -1});
 
-        int[] expectedArray = new int[]{1, 2, 4, 8};
-        assertTrue("Массивы не равны", java.util.Arrays.equals(resultArray, expectedArray)); // тест на правильную сортировку (конечный результат)
+        int[] expectedArray = new int[]{-1, 2, 4, 8};
+        assertTrue(java.util.Arrays.equals(resultArray, expectedArray)); // тест на правильную сортировку (конечный результат)
 
         for (int x = 0; x < resultArray.length; x++)
-            assertFalse("Элемент массива больше 10", resultArray[x] > 10); // значения элементов массива не должны быть больше 10
+            assertFalse(resultArray[x] > 10); // значения элементов массива не должны быть больше 10
 
         boolean actual = resultArray.length > 0;
         assertTrue(actual); //  размер массива не должен быть нулевым
