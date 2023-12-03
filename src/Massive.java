b8935c6264e537b66ce12029b2a59b0da142c08e
MajesticNthg <majestic_nthg@mail.ru>
2023-12-04 05:46:00 +1000

    mod.1

--- src/Massive.java
+++ src/Massive.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Massive {
+import java.sql.SQLOutput;
+import java.util.Random;
+public class Massive {
+    public static int[] randomMassive(int[] list1) {
+        boolean xchange = true;
+        int x;
+
+        while (xchange) {
+            xchange = false; // предполагаем, что массив уже отсортирован
+
+            for (int i = 0; i < list1.length - 1; i++)
+                if (list1[i] > list1[i + 1]) {
+                    // нашли элементы,
+                    // неупорядоченные по возрастанию:
+                    // меняем их местами:
+                    x = list1[i];
+                    list1[i] = list1[i + 1];
+                    list1[i + 1] = x;
+
+                    // цикл проверки массива надо продожить снова:
+                    xchange = true;
+                }
+        }
+        return list1;
+    }
+
+
+    public static void main(String[] args) {
+
+        int [] list1 = new int[] { 2 , 4, 8, 1 };
+
+        System.out.println(randomMassive(list1));
+    }
 }
--- src/MassiveTest.java
+++ src/MassiveTest.java
@@ -1,4 +1,36 @@
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.Assert;
+
+
 import static org.junit.Assert.*;
+
 public class MassiveTest {
-  
+
+    @Test
+    public void randomMassive() {
+        Massive massive = new Massive();
+        int[] resultArray = massive.randomMassive(new int[]{2, 4, 8, 1});
+
+        int[] expectedArray = new int[]{1, 2, 4, 8};
+        assertTrue("Массивы не равны", java.util.Arrays.equals(resultArray, expectedArray)); // тест на правильную сортировку (конечный результат)
+
+        for (int x = 0; x < resultArray.length; x++)
+            assertFalse("Элемент массива больше 10", resultArray[x] > 10); // значения элементов массива не должны быть больше 10
+
+        boolean actual = resultArray.length > 0;
+        assertTrue(actual); //  размер массива не должен быть нулевым
+
+        for (int z = 0, y = resultArray.length - 1; z < resultArray.length / 2; z++, y--) {
+            boolean palindrome;
+            if (resultArray[z] == resultArray[y]) {
+                palindrome = true;
+            } else {
+                palindrome = false;
+            }
+            assertFalse(palindrome); // массив не должен представлять из себя палиндром
+        }
+    }
 }
+
